cfb22ec3834bc25cef1bb820d29196421a168802
Alexandra Khalimon <lexikhalimon@gmail.com>
2025-08-17 15:05:34 +0300

    update files

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,12 +1,61 @@
-import Section from "../Section/Section";
-import Container from "../Container/Container";
+import Section from '../Section/Section';
+import Container from '../Container/Container';
+import Form from '../Form/Form';
+import { getPhotos } from '../../services/photos';
+import { useState } from 'react';
+import type { Photo } from '../../types/photo';
+import PhotosGallery from '../PhotosGallery/PhotosGallery';
+import Loader from '../Loader/Loader';
+import Text from '../Text/Text';
+import Modal from '../Modal/Modal';
 
 export default function App() {
+  const [photos, setPhotos] = useState<Photo[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isEmpty, setIsEmpty] = useState(false);
+
+  const onSubmit = async (query: string) => {
+    setIsEmpty(false);
+    setIsLoading(true);
+    try {
+      const data = await getPhotos(query);
+      if (!data.length) {
+        setIsEmpty(true);
+        return;
+      }
+      setPhotos(data);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const closeModal = () => setIsModalOpen(false);
+
+  const [selectedPhoto, setSelectedPhoto] = useState<Photo | null>(null);
+  
+  const handleSelectedPhoto = (photo: Photo) => {
+    setSelectedPhoto(photo);
+    setIsModalOpen(true);
+  }
+
   return (
     <>
       <Section>
-        <Container>{/* Компоненти застосунку */}</Container>
+        <Container>
+          <Form onSubmit={onSubmit} />
+          {photos.length > 0 && <PhotosGallery photos={photos} onClick = {()=>handleSelectedPhoto} />}
+          {isLoading && <Loader />}
+          {isEmpty && <Text textAlign="center">Nothing found...</Text>}
+        </Container>
       </Section>
+      {isModalOpen && selectedPhoto &&
+        <Modal onClose={closeModal}>
+        <img src={selectedPhoto.src.large} alt={selectedPhoto.alt} />
+        </Modal>}
     </>
-  );
+  )
 }
--- src/components/PhotosGalleryItem/PhotosGalleryItem.tsx
+++ src/components/PhotosGalleryItem/PhotosGalleryItem.tsx
@@ -1,19 +1,21 @@
-import GridItem from "../GridItem/GridItem";
+import type { Photo } from '../../types/photo';
+import styles from './PhotosGalleryItem.module.css';
 
-import styles from "./PhotosGalleryItem.module.css";
+interface PhotosGalleryItemProps {
+  photo: Photo;
+  onClick: (photo:Photo) => void;
+}
 
-export default function PhotosGalleryItem() {
+export default function PhotosGalleryItem({ photo, onClick }: PhotosGalleryItemProps) {
   return (
-    <GridItem>
-      <div
-        className={styles.thumb}
-        style={{
-          backgroundColor: "avg_color",
-          borderColor: "avg_color",
-        }}
-      >
-        <img src="" alt="" />
-      </div>
-    </GridItem>
+    <div
+      className={styles.thumb}
+      style={{
+        backgroundColor: photo.avg_color,
+        borderColor: photo.avg_color,
+      }}
+    >
+      <img src={photo.src.original} alt={photo.alt} onClick={()=>onClick(photo)}/>
+    </div>
   );
 }
